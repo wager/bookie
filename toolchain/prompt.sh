100da716377264c94732f4d5845e80638df32438
Ashwin Madavan <ashwin.madavan@gmail.com>
2021-03-20 10:32:43 -0400

    Fix Bash style violations (#43)

--- toolchain/prompt.sh
+++ toolchain/prompt.sh
@@ -2,25 +2,25 @@
 set -euo pipefail
 
 cat >> ~/.bashrc << \EOF
-prompt_ip=$(curl -s https://ipinfo.io/ip)
+PROMPT_IP=$(curl -s https://ipinfo.io/ip)
 
 prompt_user() {
-    me=$(whoami)
-    echo -en "\e[0;37m$me@$prompt_ip\e[0m"
+    user="$(whoami)@${PROMPT_IP}"
+    echo -en "\e[0;37m${user}\e[0m"
 }
 
 prompt_path() {
     path=$(dirs +0)
-    echo -en "\e[0;35m$path\e[0m"
+    echo -en "\e[0;35m${path}\e[0m"
 }
 
 prompt_branch() {
     if git rev-parse --is-inside-work-tree &> /dev/null 2>&1; then
         branch=$(git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/')
-        if [ -z "$(git status --porcelain)" ]; then
-            echo -en "\e[0;32m$branch\e[0m"
+        if [[ -z "$(git status --porcelain)" ]]; then
+            echo -en "\e[0;32m${branch}\e[0m"
         else
-            echo -en "\e[0;31m$branch\e[0m"
+            echo -en "\e[0;31m${branch}\e[0m"
         fi
     fi
 }

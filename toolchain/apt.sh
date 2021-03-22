d326f47b74bb50ee477781bb879035865b5dd65a
Ashwin Madavan <ashwin.madavan@gmail.com>
2021-03-22 09:49:07 -0400

    Added dependency on git-man (#47)
    - Added --allow-downgrades flag to apt-get install.
    - Downgrade git-man dependency.

--- toolchain/apt.sh
+++ toolchain/apt.sh
@@ -9,10 +9,11 @@ sudo() {
 
 # Install system dependencies.
 sudo apt-get update --yes
-sudo DEBIAN_FRONTEND=noninteractive apt-get install --yes --no-install-recommends \
+sudo DEBIAN_FRONTEND=noninteractive apt-get install --yes --allow-downgrades --no-install-recommends \
     curl=7.68.0-1ubuntu2.4 \
     default-jdk=2:1.11-72 \
     git=1:2.25.1-1ubuntu3.1 \
+    git-man=1:2.25.1-1ubuntu3.1 \
     jq=1.6-1ubuntu0.20.04.1 \
     node-gyp=6.1.0-3 \
     npm=6.14.4+ds-1ubuntu2 \

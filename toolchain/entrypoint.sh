52944391fb673ce144cb430c51520e7ab603b061
Ashwin Madavan <ashwin.madavan@gmail.com>
2021-07-09 12:34:07 -0700

    Create entrypoint.sh to run Spark masters and workers

---
+++ toolchain/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -euo pipefail
+
+if [ "${SPARK_MODE:-}" == "master" ]; then
+    # Launch a Spark master.
+    exec env SPARK_NO_DAEMONIZE=true /opt/spark/sbin/start-master.sh
+elif [ "${SPARK_MODE:-}" == "worker" ]; then
+    # Launch a Spark worker.
+    exec env SPARK_NO_DAEMONIZE=true /opt/spark/sbin/start-slave.sh "${SPARK_MASTER_URL}"
+else
+    # Launch a Bash shell.
+    exec bin/bash
+fi

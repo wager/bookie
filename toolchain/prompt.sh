5df486ade65e4e665d2fb113a7dbd5009659a71a
Ashwin Madavan <ashwin.madavan@gmail.com>
2021-07-21 00:44:42 -0700

    Add quotes around variables in toolchain/prompt.sh

--- toolchain/prompt.sh
+++ toolchain/prompt.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 if ! [ -f ~/.bashrc ] || ! grep -q '^PS1=' ~/.bashrc; then
     cat >> ~/.bashrc << \EOF
-prompt_ip=$(curl -s https://ipinfo.io/ip)
+prompt_ip="$(curl -s https://ipinfo.io/ip)"
 
 prompt_user() {
     user="$(whoami)@${prompt_ip}"
@@ -11,13 +11,13 @@ prompt_user() {
 }
 
 prompt_path() {
-    path=$(dirs +0)
+    path="$(dirs +0)"
     echo -en "\e[0;35m${path}\e[0m"
 }
 
 prompt_branch() {
     if git rev-parse --is-inside-work-tree &> /dev/null 2>&1; then
-        branch=$(git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/')
+        branch="$(git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/')"
         if [[ -z "$(git status --porcelain)" ]]; then
             echo -en "\e[0;32m${branch}\e[0m"
         else

760c88dfaa7169e9e0cecd338f0afe5c695e9e05
Ashwin Madavan <ashwin.madavan@gmail.com>
2021-08-13 17:36:58 +0200

    Add vim to the toolchain

--- toolchain/apt.sh
+++ toolchain/apt.sh
@@ -20,7 +20,8 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get install --yes --allow-downgrades --n
     python3-pip=20.0.2-5ubuntu1.6 \
     python3-lxml=4.5.0-1ubuntu0.3 \
     sudo=1.8.31-1ubuntu1.2 \
-    tini=0.18.0-1
+    tini=0.18.0-1 \
+    vim=2:8.1.2269-1ubuntu5
 
 # Install Node.js dependencies.
 curl -sL https://deb.nodesource.com/setup_16.x | sudo bash -

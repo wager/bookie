5553d16de24370cf71b0aec767da615e035bd999
Ashwin Madavan <ashwin.madavan@gmail.com>
2021-08-13 10:23:21 +0200

    Update pre-commit.sh

--- toolchain/pre-commit.sh
+++ toolchain/pre-commit.sh
@@ -3,4 +3,4 @@ set -euo pipefail
 
 pip3 install pre-commit
 git config --global init.templateDir ~/.git-template
-pre-commit init-templatedir ~/.git-template
+~/.local/bin/pre-commit init-templatedir ~/.git-template

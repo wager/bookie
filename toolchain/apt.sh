68e7a8d2f5f8ee8d304a2d5fe81c9c6e22903653
Ashwin Madavan <ashwin.madavan@gmail.com>
2021-08-13 09:41:09 +0200

    Add dependency on python3-pip=20.0.2-5ubuntu1.5

--- toolchain/apt.sh
+++ toolchain/apt.sh
@@ -17,10 +17,11 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get install --yes --allow-downgrades --n
     jq=1.6-1ubuntu0.20.04.1 \
     python-is-python3=3.8.2-4 \
     python3-distutils=3.8.10-0ubuntu1~20.04 \
+    python3-pip=20.0.2-5ubuntu1.5 \
     python3-lxml=4.5.0-1ubuntu0.3 \
     sudo=1.8.31-1ubuntu1.2 \
     tini=0.18.0-1
 
-# Install Node.js dependency.
+# Install Node.js dependencies.
 curl -sL https://deb.nodesource.com/setup_16.x | sudo bash -
 sudo DEBIAN_FRONTEND=noninteractive apt-get install --yes nodejs=16.6.2-deb-1nodesource1

1c34cd03532255aba6600bbd1ad5ef309a10b0f3
Ashwin Madavan <ashwin.madavan@gmail.com>
2021-08-14 01:04:53 +0200

    Add buildifier to toolchain

--- toolchain/bazel.sh
+++ toolchain/bazel.sh
@@ -4,3 +4,7 @@ set -euo pipefail
 # Install Bazelisk.
 sudo curl -fsSLo /usr/local/bin/bazel https://github.com/bazelbuild/bazelisk/releases/download/v1.10.1/bazelisk-linux-amd64
 sudo chmod +x /usr/local/bin/bazel
+
+# Install Buildifier.
+sudo curl -fsSLo /usr/local/bin/buildifier https://github.com/bazelbuild/buildtools/releases/download/4.0.1/buildifier-linux-amd64
+sudo chmod +x /usr/local/bin/buildifier
